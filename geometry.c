aaff816c9fa6618613c4cea977043d3a4e93af72
Artem Ochnev <Doker13@users.noreply.github.com>
2023-02-27 11:22:18 -0500

    Fix several problems in geometry.c

--- geometry.c
+++ geometry.c
@@ -1,76 +1,59 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main() {
   char *file = "geom.txt";
-  char standart[7]="circle";
-  char str[100];
+  char standart[]="circle";
   FILE *fp = fopen(file, "r");
-  while (!feof(fp)) {
-    char si1[2];
-    char si2[2];
-    char ik[2];
-    double x;
-    double y;
-    double r;
-    fscanf(fp, "%s %c%lg %lg%c %lg%c", str, si1, &x, &y, ik, &r, si2);
-    char pref[200];
-    sprintf(pref,"%s %s%g %g%s %g%s", str, si1, x, y, ik, r, si2);
-    int n = strcmp(standart, str);
+  char str[100];
+  char circ[20];
+  while (fscanf(fp, "%s ", circ)==1) {
+    fgets(str, 100, fp);
+    
+    char data[20];
+    int i=0;
+    while(circ[i]!='\0'){
+      data[i]=tolower(circ[i]);
+      i++;
+    }
+    int n = strcmp(standart, data);
     if (n!=0){
-      printf("%s %s%g %g%s %g%s\n^\nError at column 0: expected 'circle'\n",str, si1, x, y, ik, r, si2);
+      printf("%s %sError at column 0: expected 'circle'\n", circ, str);
       break;
     }
     
-    int ret = 1;
-    for (int j = 8; pref[j] != ')'; j++){
-      if ((pref[j] >= 48 && pref[j] <= 57) || pref[j] == '.' || pref[j] == ',' || pref[j] == ' ')
-        ret = 0;
-        else{
-          printf("Error: expected double\n");
-          ret = 1;
+    if (str[0]!='('){
+      printf("%s %sError at column 8: expected '('\n", circ, str);
+      break;
+    }
+    
+    int k=1;
+    while (1){
+      if (str[k+2]=='\0' && str[k]==')'){
+        break;
+      }
+      else if (str[k]=='\0') {
+        k+=6;
+        printf("%s %sError at column %d: out of range\n", circ, str, k);
+        break;
+      }
+      else{
+        k++;
+      }
+    }
+    
+    int ind=1;
+    for (int i = 1; str[i] != ')'; i++) {
+      if ((str[i] >= 48 && str[i] <= 57) || str[i] == '.' || str[i] == ',' || str[i] == ' ')
+          ind++;
+      else {
+          ind+=8;
+          printf("%s %sError at column %d: error in the argument field\n", circ, str, ind);
           break;
-        }
-    }
-    
-    if (ret==1){
-    break;
-    }
-    
-    if (si1[0]!=40){
-      int i=0;
-      printf("%s\n", pref);
-      while(si1[0]!=pref[i]){
-        i++;
-        printf(" ");
       }
-      i++;
-      printf("^\nError at column %d: expected '('\n", i);
-      break;
     }
-    if (ik[0]!=44){
-      int i=0;
-      printf("%s\n", pref);
-      while(ik[0]!=pref[i]){
-        i++;
-        printf(" ");
-      }
-      i++;
-      printf("^\nError at column %d: expected ','\n", i);
-      break;
-    }
-    if (si2[0]!=41){
-      printf("%s\n", pref);
-      int i=strlen(pref);
-      int count =i;
-      while(count!=1){
-        count--;
-        printf(" ");
-      }
-      printf("^\nError at column %d: expected ')'\n", i-1);
-      break;
-    }
-    printf("All done\n");
+
   }
   fclose(fp);
   return 0;
