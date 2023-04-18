6e3bbd7ac012fefacd9e50a98412b01a9f325651
Artem Ochnev <Doker13@users.noreply.github.com>
2023-04-17 23:24:03 -0400

    Format files

--- src/libgeometry/PaS.h
+++ src/libgeometry/PaS.h
@@ -7,8 +7,8 @@ typedef struct circle {
     double R;
 } circle;
 
-double x_reader(char *str);
-double y_reader(char *str);
-double r_reader(char *str);
+double x_reader(char* str);
+double y_reader(char* str);
+double r_reader(char* str);
 int square(double r);
 int perimetr(double r);
